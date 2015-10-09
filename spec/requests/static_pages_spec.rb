3cd6c93e926d5d3cae5bc60d21a13863e48bdd67
LB426 <odissey@rambler.ru>
2015-10-09 22:14:57 +0300

    изучал sass

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,16 +5,28 @@ describe "Static pages" do
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
+      visit root_path
+      expect(page).to have_content('Добро пожаловать в "Учебный центр ПИК"')
     end
   end
 
   describe "help page" do
 
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_content('Help')
     end
   end
+
+  describe "contact page" do
+    it "should have the contet 'Contact'" do
+      visit contact_path
+      expect(page).to have_content('Contact')
+    end
+    it "should have the title 'Contact'" do
+      visit contact_path
+      expect(page).to have_title("Учебный центр ПИК | Contact")
+    end
+  end
+
 end
