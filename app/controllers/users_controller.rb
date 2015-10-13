21278cc6ca61d6693c54eb7c142413259732d6d8
LB426 <admin@tih.kuban.ru>
2015-10-13 17:19:19 +0300

    делаю логин

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
@@ -11,7 +12,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-    # Handle a successful save.
+      log_in @user
+      flash[:success] = "Пользователь создан успешно!"
+      redirect_to @user
     else
       render 'new'
     end
