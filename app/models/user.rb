ac7df9b95d92de7eb4da4da332df5cb7f9c8c847
LB426 <admin@tih.kuban.ru>
2015-10-21 17:44:21 +0300

    сделал about

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_many :microposts, dependent: :destroy
   
   attr_accessor :remember_token, :activation_token, :reset_token
-  before_save :downcase_email
+  before_save   :downcase_email
   before_create :create_activation_digest
   
   validates :name,   presence: true, length: { maximum: 50 }
