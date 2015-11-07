79c343f7fcc66023b214cfb7ba5574b5f14010b1
centrpik <centrpik@ror.tih.kuban.ru>
2015-11-07 17:45:39 +0300

    косяк при деплое тестов. если есть тест но нет вопросов к нему

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -6,7 +6,10 @@ class TestsController < ApplicationController
   
   def index
     @tests = Test.all
-   
+    if @tests.nil?
+      flash[:danger] = "ERROR! Не создано ни одного теста"
+      redirect_to root_path
+    end
   end
 
   def new
