aba0e8616ff9c940aa9b344e59e4ad3664fcf15c
LB426 <admin@tih.kuban.ru>
2015-11-10 08:07:01 +0300

    делаю видео

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 # coding: utf-8
 Rails.application.routes.draw do
   root                'static_pages#home'
-  get    'help'    => 'static_pages#help'
-  get    'about'   => 'static_pages#about'
+  get       'help' => 'static_pages#help'
+  get      'about' => 'static_pages#about'
   get    'contact' => 'static_pages#contact'
   get  'dlearning' => 'static_pages#dlearning'
-  get    'signup'  => 'users#new'
-  get    'login'   => 'sessions#new'
   get    'library' => 'static_pages#library'
-  post   'login'   => 'sessions#create'
-  delete 'logout'  => 'sessions#destroy'
+  get     'signup' => 'users#new'
+  get      'login' => 'sessions#new'
+  post     'login' => 'sessions#create'
+  delete  'logout' => 'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
