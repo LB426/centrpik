506c4cfc3560e5679d1726f3abe722f40d2bb65b
LB426 <admin@tih.kuban.ru>
2015-11-11 17:20:42 +0300

    делаю курсы

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,2 +1,54 @@
+# coding: utf-8
 class CoursesController < ApplicationController
+
+  def index
+    @courses = Course.all.order( :id => "ASC" )
+  end
+
+  def new
+    @course = Course.new
+  end
+
+  def create
+    @course = Course.new(course_params)
+    if @course.save
+      flash[:info] = "Курс создан"
+      redirect_to edit_course_path(@course)
+    else
+      flash[:danger] = "ERROR: создать курс не удалось!"
+      render 'new'
+    end
+  end
+
+  def show
+    @course = Course.find(params[:id])
+  end
+
+  def edit
+    @course = Course.find(params[:id])
+  end
+
+  def update
+    @course = Course.find(params[:id])
+    if @course.update_attributes(course_params)
+      flash[:success] = "Курс обновлён"
+      redirect_to edit_course_path(@course)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    Course.find(params[:id]).destroy
+    flash[:success] = "курс удалён"
+    redirect_to courses_path
+  end
+
+
+private
+
+  def course_params
+    params.require(:course).permit(:name, :description)
+  end
+  
 end
